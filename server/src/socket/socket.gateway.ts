f98bd624377835fa783ce6d33fbab99daa0521bf
duncan nevin <duncan.nevin@gmail.com>
2020-04-27 12:12:10 -0400

    socket listens

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,20 +1,32 @@
 import {MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse} from '@nestjs/websockets'
 import {Server} from 'socket.io'
 import {from, Observable} from 'rxjs'
-import {map} from 'rxjs/operators'
+import {delay, map} from 'rxjs/operators'
+import {TarsierLogger} from '../logger/tarsier.logger'
 
 @WebSocketGateway()
 export class SocketGateway {
   @WebSocketServer()
   server: Server
 
+  constructor(
+    private logger: TarsierLogger
+  ) {
+    logger.setContext('SocketGateway')
+  }
+
   @SubscribeMessage('events')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    this.logger.log(`Events received a message ${data}`)
+    return from([1, 2, 3]).pipe(
+      delay(500),
+      map(item => ({ event: 'events', data: item }))
+    );
   }
 
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
+    this.logger.log(`Identity received a message ${data}`)
     return data;
   }
 }
--- server/src/socket/socket.module.ts
+++ server/src/socket/socket.module.ts
@@ -1,7 +1,9 @@
 import {Module} from '@nestjs/common'
 import {SocketGateway} from './socket.gateway'
+import {LoggerModule} from '../logger/logger.module'
 
 @Module({
+  imports: [LoggerModule],
   providers: [SocketGateway]
 })
 export class SocketModule {

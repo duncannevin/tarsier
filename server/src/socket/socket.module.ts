ee40c64dc467f6dc933e56bf14133e40e9442c30
duncan nevin <duncan.nevin@gmail.com>
2020-04-27 16:20:06 -0400

    adds socket service

--- server/src/socket/socket.module.ts
+++ server/src/socket/socket.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common'
 import {SocketGateway} from './socket.gateway'
 import {LoggerModule} from '../logger/logger.module'
+import { SocketService } from './socket.service';
 
 @Module({
   imports: [LoggerModule],
-  providers: [SocketGateway]
+  providers: [SocketGateway, SocketService]
 })
 export class SocketModule {
 }

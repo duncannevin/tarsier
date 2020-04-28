2fd8e0deea9862e93001a5a5a858fded2e45cc70
duncan nevin <duncan.nevin@gmail.com>
2020-04-28 14:57:25 -0400

    ws exception filter

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -3,16 +3,14 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsException,
-  WsResponse
 } from '@nestjs/websockets'
 import {Server} from 'socket.io'
-import {from, Observable} from 'rxjs'
-import {delay, map} from 'rxjs/operators'
 import {TarsierLogger} from '../logger/tarsier.logger'
 import {InitializeDto} from './dto/initialize.dto'
-import {UsePipes} from '@nestjs/common'
-import {WsValidationPipe} from './pipe/ws.validation.pipe'
+import {UseFilters, UsePipes} from '@nestjs/common'
+import {SocketService} from './socket.service'
+import {InitializeValidationPipe} from './pipe/initialize.validation.pipe'
+import {WsExceptionFilter} from './filter/ws.exception.filter'
 
 @WebSocketGateway()
 export class SocketGateway {
@@ -20,31 +18,20 @@ export class SocketGateway {
   server: Server
 
   constructor(
-    private logger: TarsierLogger
+    private logger: TarsierLogger,
+    private socketService: SocketService
   ) {
     logger.setContext('SocketGateway')
   }
 
+  @UseFilters(new WsExceptionFilter())
   @SubscribeMessage('initialize-environment')
-  @UsePipes(new WsValidationPipe())
-  initializeEnvironment(@MessageBody() initializeDto: InitializeDto) {
-    this.logger.log(`Initialize Environment Triggered: ${JSON.stringify(initializeDto)}`)
+  @UsePipes(new InitializeValidationPipe())
+  initializeEnvironment(
+    @MessageBody() initializeDto: InitializeDto
+  ) {
+    this.logger.log(`Initialize Environment Triggered: ${JSON.stringify(initializeDto.language)}`)
 
-    return initializeDto
-  }
-
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<string>> {
-    this.logger.log(`Events received a message ${data}`)
-    return from([1, 2, 3]).pipe(
-      delay(500),
-      map(item => ({ event: 'events', data: `${item}: ${data}` }))
-    );
-  }
-
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    this.logger.log(`Identity received a message ${data}`)
-    return data;
+    return this.socketService.initializeEnvironment(initializeDto)
   }
 }
--- server/src/socket/socket.service.ts
+++ server/src/socket/socket.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import {InitializeDto} from './dto/initialize.dto'
 
 @Injectable()
-export class SocketService {}
+export class SocketService {
+  initializeEnvironment(initializeDto: InitializeDto) {
+    return `>>> ${initializeDto.language}`
+  }
+}

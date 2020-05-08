36ec40f931d846513aac45e1707707207f511519
Duncan Nevin <duncan.nevin@gmail.com>
2020-05-08 17:45:44 -0400

    implements is same user guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import {Controller, Delete, Get, Param, Post, UseGuards} from '@nestjs/common'
+import {Controller, Delete, Get, Param, Post, Req, UseGuards} from '@nestjs/common'
 import {UsersService} from './users.service'
 import {User} from './user.entity'
 import {Body} from '@nestjs/common/decorators/http/route-params.decorator'
 import {CreateUserDto} from './dto/create-user.dto'
 import {LoginUserDto} from './dto/login-user.dto'
 import {JwtAuthGuard} from '../guards/jwt-auth.guard'
+import {IsSameUserGuard} from '../guards/is-same-user.guard'
 
 @Controller('users')
 export class UsersController {
@@ -19,6 +20,7 @@ export class UsersController {
     return this.usersService.findAll()
   }
 
+  @UseGuards(JwtAuthGuard, IsSameUserGuard)
   @Get('/:id')
   findOne(@Param('id') id): Promise<User> {
     return this.usersService.findOne(id)
@@ -29,6 +31,7 @@ export class UsersController {
     return this.usersService.create(createUserDto)
   }
 
+  @UseGuards(JwtAuthGuard, IsSameUserGuard)
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
     return this.usersService.remove(id)
